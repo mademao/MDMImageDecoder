92bc74178bd03b3ebb6ce67804355dc7788f5683
mademao <ismademao@gmail.com>
2019-03-22 11:06:57 +0800

    Commit Project File

---
+++ MDMImageDecoder/MDMImageDecoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MDMImageDecoder
+//
+//  Created by mademao on 2019/3/22.
+//  Copyright © 2019 mademao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MDMImageDecoder/MDMImageDecoder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MDMImageDecoder
+//
+//  Created by mademao on 2019/3/22.
+//  Copyright © 2019 mademao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
